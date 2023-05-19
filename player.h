69dd6ed6291ea306d58ae7bb8c80b9cc24c2ac7e
Gogumalang <22000269@handong.ac.kr>
2023-05-19 17:26:03 +0900

    player.h : fix comment

--- player.h
+++ player.h
@@ -20,38 +20,38 @@ typedef struct node{
 
 int account_list[100];
 
+// 미완성 
 void reset_data(node *t,node *h); // 미완성 - 커밋 
         void random_stat_allocation(int *s);
         void open_demo_file(node *h);
 
 
-//void set_data(node *h);
-node *init(); // 재활용 가능 
-node *find_name(node *h,char name[]); // 재활용 가능 
-//void add_player(node *h);
-void update_player(node *h); // 업데이트
-void delete_player(node *h); // 업데이트 
-void open_file(node *h); // 업데이트 - 커밋 // 수정
-void server_filesave(node *h); // 업데이트 -커밋 // 수정
-void data_player(node *t); // 업데이트 
-void read_player(node *h); // 업데이트 // 완성 - 커밋
-void read_squad(node *h,int n); // 업데이트 // 완성 - 커밋
-void buy_player(node *h,int *account,int n); // 업데이트 //완성
-void sell_player(node *h,int *account,int n); //업데이트 // 완성 
-void cmp_player(node *h); // 업데이트 // 완성 
+// 완성 : 내용은 완성되었지만, 출력 부분의 수정이 필요한 함수들 입니다. 
+//      출력 완성을 했으면 각 함수에 '//' 옆에 '출력부분 수정' 이라고 표시해주시면 감사하겠습니다! 
+node *init(); // 링크드 리스트 선언 함수 // 
+node *find_name(node *h,char name[]); // 이름을 찾아서 해당 노드를 리턴하는 함수 // 
+
+void update_player(node *h); // 선수 정보 수정 함수 // 
+void delete_player(node *h); // 선수 정보 삭제 함수 //
+void open_file(node *h); //  선수 정보를 리스트에 저장하는 함수 //
+void server_filesave(node *h); // 리스트에 저장된 선수 정보를 파일에 저장하는 함수 // 
+void data_player(node *t); // 선수들의 상세 정보를 출력하는 함수 // 
+void read_player(node *h); // 선수 정보를 포지션 별로 구분하여 볼 수 있는 함수 // 
+void read_squad(node *h,int n); // 자신의 스쿼드를 볼 수 있는 함수 //
+void buy_player(node *h,int *account,int n); // 선수 구입 함수 // 
+void sell_player(node *h,int *account,int n); // 선수 판매 함수 //
+void cmp_player(node *h); // 선수 비교 함수 //
 
 
-int server_login(); // 업데이트 // 완성 - 커밋 
-int select_server(); // 재활용 가능 //완성 - 커밋 
-int select_client(); // 재활용 가능 // 완성 - 커밋 
-void deallocation(node *h); // 재활용 가능 //완성 -커밋 
+int server_login(); // 서버 로그인 함수 //
+int select_server(); // 서버 메뉴 함수 //
+int select_client(); // 유저 메뉴 함수 //
+void deallocation(node *h); // 링크드 리스트 할당을 해제 함수 //
 
-void read_all_players(node *h); // 새로 구현함 // 완성 - 커밋 
+void read_all_players(node *h); // 모든 선수 정보를 보는 함수 //
 
-
-//커밋
-int sign_in_or_up(); // 회원가입 로그인 선택하기 - 구현완료 // 수정 
-void sign_in(); // 유저로서 회원가입 하기 - 구현완료  // 수정 
-int sign_up(); // 유저로서 로그인 하기 - 구현완료  // 수정 
-int get_account(int n); // 해당 계좌 읽어들이기 - 구현완료  // 수정 
-void save_account(int u,int n); // 계좌파일로 업로드 - 구현완료 // 수정
+int sign_in_or_up(); // 회원가입 로그인 선택하기 //
+void sign_in(); // 유저로서 회원가입 하기 //
+int sign_up(); // 유저로서 로그인 하기 //
+int get_account(int n); // 해당 계좌 읽어들이기 //
+void save_account(int u,int n); // 계좌파일로 업로드 //
